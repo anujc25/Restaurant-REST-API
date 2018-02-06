65d0a16ae74c1cfa8392817aaf16675df8e86ed7
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-02-06 11:13:15 -0800

    update unit tests

--- api/test.js
+++ api/test.js
@@ -1,13 +1,36 @@
 var supertest = require("supertest");
 var should = require("should");
+var mongoose = require('mongoose');
 
-// This agent refers to PORT where program is runninng.
 
 var server = supertest.agent("http://localhost:3000/zappos/api/v1");
 
 var restaurant_id = "";
 
 
+describe('Test Database', function() {
+  //Before starting the test, create a database connection
+  //Once a connection is established invoke done()
+  it("Mongodb connection check",function (done) {
+    mongoose.connect('mongodb://localhost/anuj_restaurant_api');
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', function() {
+      done();
+    });
+  });
+
+  //drop database and close connection
+  after(function(done){
+    mongoose.connection.db.dropDatabase(function(){
+      mongoose.connection.close(done);
+    });
+  });
+
+});
+
+
+// Test Cases for Restaurant API
 describe("Test Cases: Restaurant POST GET",function(){
 
   it("POST should store new restaurant info in database and respond 201 created",function(done){
@@ -127,7 +150,7 @@ describe("Test Cases: Restaurant POST GET",function(){
 });
 
 
-
+// Test Case for Menu API
 describe("Test Cases: Menu POST GET",function(){
 
     it("POST should add new menu to restaurants menu list in database and respond 200 OK",function(done){
@@ -219,7 +242,7 @@ describe("Test Cases: Menu POST GET",function(){
 
 
 
-
+// Test Case for Menu Item API
 describe("Test Cases: Menu Items POST GET",function(){
 
     it("POST should add new menuitem to restaurants given menu in database and respond 200 OK",function(done){
@@ -241,8 +264,44 @@ describe("Test Cases: Menu Items POST GET",function(){
         done();
       });
     });
+
+    it("POST should return 404 Not Found if Menu does not exists while adding item",function(done){
+  
+      var menu_item = {
+            "item_name": "Coffee Latte",
+            "Description": "coffee with the best flovour of latte",
+            "price"	: 3
+        };
+  
+      server
+      .post("/restaurants/" + restaurant_id + "/menus/abc/items")
+      .send(menu_item)
+      .expect("Content-type",/json/)
+      .expect(404) // THis is HTTP response
+      .end(function(err,res){      
+        res.status.should.equal(404);
+        res.body.should.be.a.Object;
+        done();
+      });
+    });
+
   
     it("GET should return all menu items for given menu",function(done){
+      
+      server
+      .get("/restaurants/" + restaurant_id + "/menus/coffee/items")
+      .expect("Content-type",/json/)
+      .expect(200) // THis is HTTP response
+      .end(function(err,res){
+        res.status.should.equal(200);
+        res.body.should.be.a.Array;
+        done();
+      });
+      
+    });
+
+      
+    it("GET should return 404 Not Found if menu does not exists while fetching items",function(done){
       
       server
       .get("/restaurants/" + restaurant_id + "/menus/coffee/items")
@@ -267,7 +326,7 @@ describe("Test Cases: Menu Items POST GET",function(){
 
 describe("Test Cases: DELETE Menu, Restaurant",function(){
 
-    it("DELETE should return 200 OK, while deleting restaurant with it's id",function(done){
+    it("DELETE should return 200 OK, while deleting menu with MenuID",function(done){
       
         server
         .delete("/restaurants/" + restaurant_id + "/menus/hot-drinks")
@@ -280,6 +339,19 @@ describe("Test Cases: DELETE Menu, Restaurant",function(){
         
       });
 
+      it("DELETE should return 404 Not Found, while deleting menu with MenuID and if restaurant or menu does not exist for that id. ",function(done){
+      
+        server
+        .delete("/restaurants/" + restaurant_id + "/menus/random-text-menu")
+        .expect("Content-type",/json/)
+        .expect(404) // THis is HTTP response
+        .end(function(err,res){
+          res.status.should.equal(404);      
+          done();
+        });
+        
+      });
+
       it("DELETE should return 200 OK, while deleting restaurant with it's id",function(done){        
         server
         .delete("/restaurants/" + restaurant_id)
@@ -287,6 +359,18 @@ describe("Test Cases: DELETE Menu, Restaurant",function(){
         .expect(200) // THis is HTTP response
         .end(function(err,res){
           res.status.should.equal(200);      
+          done();
+        });
+        
+      });
+
+      it("DELETE should return 404 Not Found, while deleting restaurant, if restaurant does not exist for that id. ",function(done){        
+        server
+        .delete("/restaurants/random-restaurant-id")
+        .expect("Content-type",/json/)
+        .expect(404) // THis is HTTP response
+        .end(function(err,res){
+          res.status.should.equal(404);      
           done();
         });
         
